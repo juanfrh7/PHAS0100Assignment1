9ae5c8e7077d2c8c2dec844dbd938df2577d4665
Juan Francisco <juanfrh7@gmail.com>
2022-02-11 19:12:36 +0000

    created the structure for the DataStructure header

---
+++ Code/Lib/DataStructure.h
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#ifndef DataStructure_h
+#define DataStructure_h
+
+class DataStructure {
+    public:
+        DataStructure();
+        DataStructure(const int& rows, const int& columns);
+        DataStructure(const int& rows, const int& columns, const int& cells);
+        DataStructure(std::string);
+        ~DataStructure();
+        void Create2DGrid();
+        void SetRandomlyAlive();
+        void PrintGrid();
+        void GetCellContent(int x, int y);
+        void SetCellContent(int x, int y);
+        void CountAliveNeighbourCell(int x, int y);
+
+    private:
+        int columns_;
+        int rows_;
+        int cells_;
+        std::vector<std::vector<char>> vec2D_;
+};
+
+#endif
