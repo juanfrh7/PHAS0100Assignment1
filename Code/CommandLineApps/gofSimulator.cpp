942558be40e67c44c140a2fdffc0ba0cf173a475
Juan Francisco <juanfrh7@gmail.com>
2022-02-20 17:24:22 +0000

    create the gofSimulator command line

--- Code/CommandLineApps/gofSimulator.cpp
+++ Code/CommandLineApps/gofSimulator.cpp
@@ -16,8 +16,10 @@
 #include <golExceptionMacro.h>
 #include <iostream>
 #include "DataStructure.h"
+#include "gameEvolution.h"
 #include <memory>
 #include <utility>    
+#include <vector>    
 
 // Example, header-only library, included in project for simplicity's sake.
 #include <Eigen/Dense>
@@ -28,6 +30,8 @@
  */
 int main(int argc, char* argv[])
 {
+
+
     // Check the number of parameters
     if (argc == 1) {
       std::cerr << "In order to use the command line, pick between the next options:" << std::endl;
@@ -35,18 +39,30 @@ int main(int argc, char* argv[])
       std::cerr << "or" << std::endl;
       std::cerr << argv[0] << " filepath.txt" << std::endl;
     }
-    if (argc == 2) {
-        DataStructure initial(argv[1]);
-        initial.PrintGrid();
+    if (argc == 3) {
+        std::shared_ptr<DataStructure> initial = std::make_shared<DataStructure>(argv[1]);
+        gameEvolution evolution(initial);
+        evolution.CreateGridFromFile();
+        long arg = strtol(argv[2], NULL, 10);
+        for(int i = 0; i < arg; ++i){
+          std::cout << "Step " << i << std::endl;
+          evolution.PrintGrid();
+          evolution.TakeStep();
+      }
     }
-    if (argc == 4) {
-      int ia1 = atoi(argv[1]);
-      int ia2 = atoi(argv[2]);
-      int ia3 = atoi(argv[3]); 
-      DataStructure initial(ia1, ia2, ia3);
-      initial.Create2DGrid();
-      initial.SetRandomlyAlive
-      initial.PrintGrid();
+    if (argc == 5) {
+        long arg1 = strtol(argv[1], NULL, 10);
+        long arg2 = strtol(argv[2], NULL, 10);
+        long arg3 = strtol(argv[3], NULL, 10);
+        long arg4 = strtol(argv[4], NULL, 10);
+        std::shared_ptr<DataStructure> initial = std::make_shared<DataStructure>(arg1, arg2, arg3);
+        gameEvolution evolution(initial);
+        evolution.CreateGrid();
+        for(int i = 0; i < arg4; ++i){
+          std::cout << "Step " << i << std::endl;
+          evolution.PrintGrid();
+          evolution.TakeStep();
+      }
     }
     return 0;
 }
