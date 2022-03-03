2223d6c83854f5f329eb177a228a192c89efb51d
Juan Francisco <juanfrh7@gmail.com>
2022-03-03 11:59:35 +0000

    improve readability / add comments to the com line

--- Code/CommandLineApps/gofSimulator.cpp
+++ Code/CommandLineApps/gofSimulator.cpp
@@ -23,17 +23,21 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <cstring>
 
-// Example, header-only library, included in project for simplicity's sake.
-#include <Eigen/Dense>
-#include <unsupported/Eigen/NonLinearOptimization>
+/*
+This command line allow the user to choose between the following two options:
+  1: specifying a text file input or
+  2: starting with random cell contents for the initial conditions.
 
-/**
- * \brief Demo file to check that includes and library linkage is correct.
+The app also has an argument to control the number of generations to simulate.
  */
+
 int main(int argc, char* argv[])
 {
     // Check the number of parameters
+
+    //only running executable
     if (argc == 1) {
       std::cerr << "In order to use the gofSimulator command line, pick between the next options:" << std::endl;
       std::cerr << "  1) "<< argv[0] << " row column cells iterations" << std::endl;
@@ -42,6 +46,7 @@ int main(int argc, char* argv[])
       std::cerr << "You should specify in the correct order the number of columns, rows, alive cells, iterations and filepath (filepath should be between \"\")" << std::endl;
     }
 
+    //define the help argument
     if (argc == 2 && (strcmp(argv[1],"--help") == 0 || strcmp(argv[1],"-h") == 0)) {
       std::cout << "In order to use the gofSimulator command line, pick between the next options:" << std::endl;
       std::cout << "  1) "<< argv[0] << " row column cells iterations" << std::endl;
@@ -50,12 +55,19 @@ int main(int argc, char* argv[])
       std::cout << "You should specify in the correct order the number of columns, rows, alive cells, iterations and filepath (filepath should be between \"\")" << std::endl;
     }
 
+    //defines and evolves a grid given a filepath
     if (argc == 3) {
+
+      //convert commmand line arguments to correct types
+      long arg = strtol(argv[2], NULL, 10);
+
+      //define the system
       std::string path = argv[1];
       std::shared_ptr<DataStructure> initial = std::make_shared<DataStructure>(path);
       gameEvolution evolution(initial);
       evolution.CreateGridFromFile();
-      long arg = strtol(argv[2], NULL, 10);
+
+      //evolve and print the grid for each iteration
       for(int i = 0; i < arg; ++i){
         std::cout << "Step " << i << std::endl;
         evolution.PrintGrid();
@@ -64,30 +76,42 @@ int main(int argc, char* argv[])
       }
     }
 
+    //defines and prints a grid
     if (argc == 4) {
+
+      //convert commmand line arguments to correct types
       long arg1 = strtol(argv[1], NULL, 10);
       long arg2 = strtol(argv[2], NULL, 10);
       long arg3 = strtol(argv[3], NULL, 10);
+
+      //define and print the system
       DataStructure initial(arg1, arg2, arg3);
       initial.Create2DGrid();
       initial.SetRandomlyAlive();
       initial.PrintGrid();
     }
 
+    //defines and evolves a grid given the dimensions, alive cells and iterations
     if (argc == 5) {
-        long arg1 = strtol(argv[1], NULL, 10);
-        long arg2 = strtol(argv[2], NULL, 10);
-        long arg3 = strtol(argv[3], NULL, 10);
-        long arg4 = strtol(argv[4], NULL, 10);
-        std::shared_ptr<DataStructure> initial = std::make_shared<DataStructure>(arg1, arg2, arg3);
-        gameEvolution evolution(initial);
-        evolution.CreateGrid();
-        for(int i = 0; i < arg4; ++i){
-          std::cout << "Step " << i << std::endl;
-          evolution.PrintGrid();
-          evolution.TakeStep();
-          std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      }
+
+      //convert commmand line arguments to correct types
+      long arg1 = strtol(argv[1], NULL, 10);
+      long arg2 = strtol(argv[2], NULL, 10);
+      long arg3 = strtol(argv[3], NULL, 10);
+      long arg4 = strtol(argv[4], NULL, 10);
+
+      //define the system
+      std::shared_ptr<DataStructure> initial = std::make_shared<DataStructure>(arg1, arg2, arg3);
+      gameEvolution evolution(initial);
+      evolution.CreateGrid();
+
+      //evolve the system and print it 
+      for(int i = 0; i < arg4; ++i){
+        std::cout << "Step " << i << std::endl;
+        evolution.PrintGrid();
+        evolution.TakeStep();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
     }
     return 0;
 }
--- Code/CommandLineApps/stationaryPatterns.cpp
+++ Code/CommandLineApps/stationaryPatterns.cpp
@@ -23,62 +23,81 @@
 #include <vector>    
 #include <random>
 #include<cstdlib>
+#include <cstring>
 
-// Example, header-only library, included in project for simplicity's sake.
-#include <Eigen/Dense>
-#include <unsupported/Eigen/NonLinearOptimization>
-
-/**
- * \brief Demo file to check that includes and library linkage is correct.
+/*
+This command line allow the user to control the number of alive cells, the grid size and the
+number of iterations for each configuration. The command line tries out different random initial 
+conditions and for each, run the user specified number of calls.
+The program prints original and final gridif there is a repetitive pattern throught its evolution.
  */
+
 int main(int argc, char* argv[])
 {
     // Check the number of parameters
+
+    //only running executable
     if (argc == 1) {
       std::cerr << "In order to use the stationaryPatterns command line, you should specify:" << std::endl;
       std::cerr << argv[0] << " row column max_cells iterations states" << std::endl;
     }
 
+    //define the help argument
     if (argc == 2 && (strcmp(argv[1],"--help") == 0 || strcmp(argv[1],"-h") == 0)) {
         std::cerr << "In order to use the stationaryPatterns command line, you should specify:" << std::endl;
         std::cerr << argv[0] << " row column max_cells iterations states" << std::endl;
     }
+
+    //define and evolve various systems
+    //print only the ones with a repetitive pattern
     if (argc == 6) {
-        long arg1 = strtol(argv[1], NULL, 10);
-        long arg2 = strtol(argv[2], NULL, 10);
-        long arg3 = strtol(argv[3], NULL, 10);
-        long arg4 = strtol(argv[4], NULL, 10);
-        long arg5 = strtol(argv[5], NULL, 10);
+      
+      //convert commmand line arguments to correct types
+      long arg1 = strtol(argv[1], NULL, 10);
+      long arg2 = strtol(argv[2], NULL, 10);
+      long arg3 = strtol(argv[3], NULL, 10);
+      long arg4 = strtol(argv[4], NULL, 10);
+      long arg5 = strtol(argv[5], NULL, 10);
 
-        srand(time(0));
+      srand(time(0));
 
-        for(int j; j< arg5; ++j){
-          int cell = rand() % arg3;
-          std::shared_ptr<DataStructure> initial = std::make_shared<DataStructure>(arg1, arg2, cell);
-          gameEvolution evolution(initial);
-          evolution.CreateGrid();
-          auto original = evolution.ReturnVec();
-          for(int i = 0; i < arg4; ++i){
-            auto x = evolution.ReturnVec();
-            evolution.TakeStep();
-            auto y = evolution.ReturnVec();
-            std::vector<std::vector<char>> vec(arg1, std::vector<char> (arg2, '-'));
-            if(y == vec){
-                break;
-                }
-            if(x == y){
-              std::cout << "\n" << std::endl;
-              std::cout << "State " << j << " with " << cell << " alive cells at iteration 0 " << std::endl;
-              evolution.PrintOriginalGrid();
-              std::cout << "\n" << std::endl;
-              std::cout << "State " << j << " has a pattern at iteration " << i + 1 << std::endl;
-              evolution.PrintGrid();
-              std::cout << "\n" << std::endl;
-              std::cout << "-----------------------------------" << std::endl;
+      for(int j; j< arg5; ++j){
+
+        //create random number of cells, with cell_max as upper limit
+        int cell = rand() % arg3;
+
+        //define the system
+        std::shared_ptr<DataStructure> initial = std::make_shared<DataStructure>(arg1, arg2, cell);
+        gameEvolution evolution(initial);
+        evolution.CreateGrid();
+        auto original = evolution.ReturnVec();
+
+        //evolve it and check for patterns
+        for(int i = 0; i < arg4; ++i){
+          auto x = evolution.ReturnVec();
+          evolution.TakeStep();
+          auto y = evolution.ReturnVec();
+          std::vector<std::vector<char>> vec(arg1, std::vector<char> (arg2, '-'));
+
+          //if the evolved system is equal to an empty grid, dont print and skip to the next iteration
+          if(y == vec){
               break;
               }
+          
+          //if the evolved system is equal to the previous system, print original and evolved system
+          if(x == y){
+            std::cout << "\n" << std::endl;
+            std::cout << "State " << j << " with " << cell << " alive cells at iteration 0 " << std::endl;
+            evolution.PrintOriginalGrid();
+            std::cout << "\n" << std::endl;
+            std::cout << "State " << j << " has a pattern at iteration " << i + 1 << std::endl;
+            evolution.PrintGrid();
+            std::cout << "\n" << std::endl;
+            std::cout << "-----------------------------------" << std::endl;
+            break;
             }
           }
         }
+      }
   return 0;
 }
